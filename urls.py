2a768148dd2a6ec7646d1816567c4cf01db3e305
Ian Olliffe <idolliffe@gmail.com>
2025-04-25 12:39:13 -0700

    Update urls.py
    switched path to re_path

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('', views.reviewer_reporter, name='reviewer_reporter'),
+    re_path(r'^$', views.reviewer_reporter, name='reviewer_reporter'),
 ]
